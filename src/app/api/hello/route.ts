c1354e2ac6f6c728d5e6db01db38d20028e0f5bd
robsonnsbr@gmail.com <robsonnsbr@gmail.com>
2023-04-04 18:03:36 -0300

    fix: comentando função não utilizada

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export {};
 //TODO: analisar função e talvez passar usar o axios.
 
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!');
-}
+// export async function GET(request: Request) {
+//   return new Response('Hello, Next.js!');
+// }
